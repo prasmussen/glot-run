51b9df616e4dbcba97593d167a024bcdc8846711
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-04-20 22:46:12 +0200

    Update scripts

--- scripts/localhost/admin_post_users.sh
+++ scripts/localhost/admin_post_users.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X POST -d '{"token": "C32BE915-38E8-486C-96DB-28C71EA87E42"}' localhost:8090/admin/users | jq .
-curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X POST -d '{"token": "B2FF0BF0-1055-436B-B5CB-C9B485AEE029"}' localhost:8090/admin/users | jq .
+curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X POST -d '{"token": "af4c0a23-7b42-4207-b61c-3430c9637055"}' localhost:8090/admin/users | jq .
--- scripts/localhost/admin_put_languages.sh
+++ scripts/localhost/admin_put_languages.sh
@@ -5,7 +5,7 @@ curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "php", "version": "latest", "image": "glot/php:latest"}' localhost:8090/admin/languages
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "java", "version": "latest", "image": "glot/java:latest"}' localhost:8090/admin/languages
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "erlang", "version": "latest", "image": "glot/erlang:latest"}' localhost:8090/admin/languages
-curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "js", "version": "latest", "image": "glot/javascript:latest"}' localhost:8090/admin/languages
+curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "javascript", "version": "latest", "image": "glot/javascript:latest"}' localhost:8090/admin/languages
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "perl", "version": "latest", "image": "glot/perl:latest"}' localhost:8090/admin/languages
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "ruby", "version": "latest", "image": "glot/ruby:latest"}' localhost:8090/admin/languages
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "c", "version": "latest", "image": "glot/c:latest"}' localhost:8090/admin/languages
