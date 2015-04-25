14af01a5de55f68b7e61c7eb359412ed8045491a
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-04-25 13:51:09 +0200

    Use clang image for c/cpp

--- scripts/localhost/admin_put_languages.sh
+++ scripts/localhost/admin_put_languages.sh
@@ -8,6 +8,6 @@ curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "javascript", "version": "latest", "image": "glot/javascript:latest"}' localhost:8090/admin/languages
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "perl", "version": "latest", "image": "glot/perl:latest"}' localhost:8090/admin/languages
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "ruby", "version": "latest", "image": "glot/ruby:latest"}' localhost:8090/admin/languages
-curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "c", "version": "latest", "image": "glot/c:latest"}' localhost:8090/admin/languages
-curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "cpp", "version": "latest", "image": "glot/cpp:latest"}' localhost:8090/admin/languages
+curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "c", "version": "latest", "image": "glot/clang:latest"}' localhost:8090/admin/languages
+curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "cpp", "version": "latest", "image": "glot/clang:latest"}' localhost:8090/admin/languages
 curl -sv -H "Authorization: Token clumeterin" -H 'Content-type: application/json' -X PUT -d '{"name": "bash", "version": "latest", "image": "glot/bash:latest"}' localhost:8090/admin/languages
